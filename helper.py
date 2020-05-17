18ea3d65489601409c7dbfcb1d81e9d8e61ac837
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-17 18:19:42 +0530

    fix change

--- helper.py
+++ helper.py
@@ -131,12 +131,53 @@ def get_products(link, egg, milk, products):
             link = page_dets["next_page"]
     return products
 
+def get_new_products(link, egg, milk, products):
+    new_products = {}
+    new_products['egg'] = []
+    new_products['milk'] = []
+    ctr = 1
+    egg_flag = 0
+    milk_flag = 0
+    egg_ctr = 0
+    milk_ctr = 0
+    while(True):
+        print(ctr)
+        ctr = ctr+1
+        try:
+            page_dets = proplanta(link)
+        except:
+            time.sleep(10)
+            page_dets = proplanta(link)
+        if not page_dets["next_page"]:
+            break
+        else:
+            for product_link in page_dets['product_links']:
+                if egg in product_link:
+                    if product_link not in products['egg']:
+                        products['egg'].insert(egg_ctr,product_link)
+                        new_products['egg'].insert(egg_ctr,product_link)
+                        egg_ctr = egg_ctr + 1
+                    else:
+                        egg_flag = 1
+                elif milk in product_link:
+                    if product_link not in products['milk']:
+                        products['milk'].insert(milk_ctr,product_link)
+                        new_products['milk'].insert(milk_ctr,product_link)
+                        milk_ctr = milk_ctr + 1
+                    else:
+                        milk_flag = 1
+                if egg_flag and milk_flag:
+                    return new_products
+            link = page_dets["next_page"]
+    return new_products
+
 def get_prices_egg(link):
     html = getHTML(link)
     soup = BeautifulSoup(html, "html.parser")
     ret_inf = []
     product_detail = {}
     date = soup.find("td", {"class": "FARBE_KATALOG_TITEL_MITTE"}).find("span",{"class": "SCHRIFT_KATALOG_INHALT_MITTE"}).text.split("|")[0].strip()
+    date = date.replace(".", "/")
     ret_inf.append(date)
     table = soup.find("div", {"id": "NachrichtDetailInhalt"})
     details = table.findAll("tr")
@@ -144,14 +185,14 @@ def get_prices_egg(link):
     product_detail["1-DE"] = {}
     product_detail["2-DE"] = {}
 
-    product_detail["1-DE"]["Güteklasse A, Gewichtsklasse XL"] = details[1].findAll("td")[1].text.replace("\xa0", "").split(" - ")
-    product_detail["1-DE"]["Güteklasse A, Gewichtklasse L"] = details[2].findAll("td")[1].text.replace("\xa0", "").split(" - ")
-    product_detail["1-DE"]["Güteklasse A, Gewichtsklasse M"] = details[3].findAll("td")[1].text.replace("\xa0", "").split(" - ")
+    product_detail["1-DE"]["Güteklasse A, Gewichtsklasse XL"] = details[1].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
+    product_detail["1-DE"]["Güteklasse A, Gewichtklasse L"] = details[2].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
+    product_detail["1-DE"]["Güteklasse A, Gewichtsklasse M"] = details[3].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
     product_detail["1-DE"]["Marktentwicklung"] = details[4].findAll("td")[1].text.replace("\xa0", "")
 
-    product_detail["2-DE"]["Güteklasse A, Gewichtsklasse XL"] = details[6].findAll("td")[1].text.replace("\xa0", "").split(" - ")
-    product_detail["2-DE"]["Güteklasse A, Gewichtklasse L"] = details[7].findAll("td")[1].text.replace("\xa0", "").split(" - ")
-    product_detail["2-DE"]["Güteklasse A, Gewichtsklasse M"] = details[8].findAll("td")[1].text.replace("\xa0", "").split(" - ")
+    product_detail["2-DE"]["Güteklasse A, Gewichtsklasse XL"] = details[6].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
+    product_detail["2-DE"]["Güteklasse A, Gewichtklasse L"] = details[7].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
+    product_detail["2-DE"]["Güteklasse A, Gewichtsklasse M"] = details[8].findAll("td")[1].text.replace("\xa0", "").replace(",",".").split(" - ")
     product_detail["2-DE"]["Marktentwicklung"] = details[9].findAll("td")[1].text.replace("\xa0", "")
     ret_inf.append(product_detail)
     return ret_inf
@@ -162,6 +203,7 @@ def get_prices_milk_butter(link):
     ret_inf = []
     product_detail = {}
     date = soup.find("td", {"class": "FARBE_KATALOG_TITEL_MITTE"}).find("span",{"class": "SCHRIFT_KATALOG_INHALT_MITTE"}).text.split("|")[0].strip()
+    date = date.replace(".", "/")
     ret_inf.append(date)
 
     tables = soup.find("div", {"id": "NachrichtDetailInhalt"})
@@ -180,36 +222,36 @@ def get_prices_milk_butter(link):
     product_detail["Hannover"]["Tagespreis Blockware"] = ['', '', '', '']
     product_detail["Hannover"]["Tagespreis Brotware"] = ['', '', '', '']
     
-    product_detail["Allgäu"]["Markenbutter_geformt"][0] = table1[1].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Markenbutter_geformt"][1] = table1[1].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Markenbutter_geformt"][2] = table1[1].findAll("td")[3].text.replace("\xa0", "")
+    product_detail["Allgäu"]["Markenbutter_geformt"][0] = table1[1].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Markenbutter_geformt"][1] = table1[1].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Markenbutter_geformt"][2] = table1[1].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
 
-    product_detail["Allgäu"]["Markenbutter_lose"][0] = table1[2].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Markenbutter_lose"][1] = table1[2].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Markenbutter_lose"][2] = table1[2].findAll("td")[3].text.replace("\xa0", "")
+    product_detail["Allgäu"]["Markenbutter_lose"][0] = table1[2].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Markenbutter_lose"][1] = table1[2].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Markenbutter_lose"][2] = table1[2].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
 
-    product_detail["Allgäu"]["Allgäuer Emmentaler"][0] = table1[3].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Allgäuer Emmentaler"][1] = table1[3].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Allgäuer Emmentaler"][2] = table1[3].findAll("td")[3].text.replace("\xa0", "")
+    product_detail["Allgäu"]["Allgäuer Emmentaler"][0] = table1[3].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Allgäuer Emmentaler"][1] = table1[3].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Allgäuer Emmentaler"][2] = table1[3].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
 
-    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][0] = table1[4].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][1] = table1[4].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][2] = table1[4].findAll("td")[3].text.replace("\xa0", "")
+    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][0] = table1[4].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][1] = table1[4].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Allgäu"]["Emmentaler und Viereckhartkäse"][2] = table1[4].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
     try:
-        product_detail["Allgäu"]["Kleinlimburger"][0] = table1[5].findAll("td")[1].text.replace("\xa0", "")
-        product_detail["Allgäu"]["Kleinlimburger"][1] = table1[5].findAll("td")[2].text.replace("\xa0", "")
-        product_detail["Allgäu"]["Kleinlimburger"][2] = table1[5].findAll("td")[3].text.replace("\xa0", "")
+        product_detail["Allgäu"]["Kleinlimburger"][0] = table1[5].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+        product_detail["Allgäu"]["Kleinlimburger"][1] = table1[5].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+        product_detail["Allgäu"]["Kleinlimburger"][2] = table1[5].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
     except:
         pass
-    product_detail["Hannover"]["Tagespreis Blockware"][0] = table2[2].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Blockware"][1] = table2[2].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Blockware"][2] = table2[2].findAll("td")[3].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Blockware"][3] = table2[2].findAll("td")[4].text.replace("\xa0", "")
+    product_detail["Hannover"]["Tagespreis Blockware"][0] = table2[2].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Blockware"][1] = table2[2].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Blockware"][2] = table2[2].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Blockware"][3] = table2[2].findAll("td")[4].text.replace("\xa0", "").replace(",",".")
 
-    product_detail["Hannover"]["Tagespreis Brotware"][0] = table2[3].findAll("td")[1].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Brotware"][1] = table2[3].findAll("td")[2].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Brotware"][2] = table2[3].findAll("td")[3].text.replace("\xa0", "")
-    product_detail["Hannover"]["Tagespreis Brotware"][3] = table2[3].findAll("td")[4].text.replace("\xa0", "")
+    product_detail["Hannover"]["Tagespreis Brotware"][0] = table2[3].findAll("td")[1].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Brotware"][1] = table2[3].findAll("td")[2].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Brotware"][2] = table2[3].findAll("td")[3].text.replace("\xa0", "").replace(",",".")
+    product_detail["Hannover"]["Tagespreis Brotware"][3] = table2[3].findAll("td")[4].text.replace("\xa0", "").replace(",",".")
     ret_inf.append(product_detail)
     return ret_inf
 
@@ -218,18 +260,21 @@ def make_link_file():
     link = "https://www.proplanta.de/markt-und-preis/"
     with open("links.json", "r") as f:
         product_links = json.load(f)
+    new_products = get_new_products("https://www.proplanta.de/markt-und-preis/", "aktuelle-eierpreise", "butterpreise-und-kaesepreise", product_links )
     product_links = get_products(link, "aktuelle-eierpreise", "butterpreise-und-kaesepreise", product_links)
     with open("links.json", "w") as f:
         json.dump(product_links, f)
+    return new_products
 
 
 def driver():
-    make_link_file()
-    with open("links.json", "r") as f:
-        product_links = json.load(f)
-        butter_milk_list = product_links["milk"]
-        eggs_list = product_links["egg"]
-    
+    product_links = make_link_file()
+    butter_milk_list = product_links["milk"]
+    eggs_list = product_links["egg"]
+    # with open("links.json", "r") as f:
+    #     product_links = json.load(f)
+    #     butter_milk_list = product_links["milk"]
+    #     eggs_list = product_links["egg"]
     butter_milk_inf = []
     for butter_milk in butter_milk_list:
         try:
@@ -281,15 +326,3 @@ def driver():
             pass
     
     gsheet_load(butter_milk_inf, eggs_inf)
-
-
-
-# driver()
-
-
-# driver()
-# pprint(get_prices_egg("https://www.proplanta.de/markt-und-preis/rheinische-warenboerse/aktuelle-eierpreise-08-05-2020_notierungen1588957359.html"))
-# pprint(proplanta("https://www.proplanta.de/markt-und-preis/butterpreise_notierungen_r418a25s0_seite_1_von_17.html"))
-
-# pprint(get_prices_milk_butter("https://www.proplanta.de/markt-und-preis/agrarmarkt-berichte/butterpreise-und-kaesepreise-vom-20-12-2017_notierungen1513793071.html"))
-# pprint(get_prices_milk_butter("https://www.proplanta.de/markt-und-preis/agrarmarkt-berichte/butterpreise-und-kaesepreise-vom-13-05-2020_notierungen1589389483.html"))
